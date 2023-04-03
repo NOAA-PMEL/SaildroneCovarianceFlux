8ea58f9169456d5dde254fb381fac009a7dd789f
Jack Reeves Eyre <jack.reeveseyre@gmail.com>
2023-04-03 08:53:36 -0400

    Updates file paths in config file.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 
-repo_dir = os.path.expanduser('~/Documents/code/SaildroneCovarianceFlux/')
-data_dir = os.path.expanduser('~/Documents/Data/Saildrone/')
+repo_dir = os.path.expanduser('~/Documents/code/saildrone/' +
+                              'SaildroneCovarianceFlux/')
+data_dir = os.path.expanduser('~/Data/ocean_obs/saildrone/')
 plot_dir = os.path.expanduser('~/Documents/plots/')
